51a7d44cb4c2b7f5e0d402b365104984a298803f
Alexander Belovitksy <mirrexone@gmail.com>
2024-11-14 12:43:05 +0300

    Add script for init db and users in one cotainer

--- internal/app/store/sqlstore/store_test.go
+++ internal/app/store/sqlstore/store_test.go
@@ -12,7 +12,7 @@ var (
 func TestMain(m *testing.M) {
 	databaseURL = os.Getenv("DATABASE_URL")
 	if databaseURL == "" {
-		databaseURL = "user=crucial password=j host=localhost dbname=restapi_test sslmode=disable"
+		databaseURL = "user=mirrex password=j host=localhost dbname=restapi_test sslmode=disable"
 	}
 
 	os.Exit(m.Run())
